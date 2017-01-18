83ded89ceea15793722321f2f0826818280ff034
Kenji.Sadakane <SADA.732738686237@GMAIL.COM>
2017-01-18 18:43:51 +0900

    外部記憶装置への書き込み権限要求を削除

--- app/src/main/java/com/keepingrack/tagmusicplayer/MainActivity.java
+++ app/src/main/java/com/keepingrack/tagmusicplayer/MainActivity.java
@@ -33,12 +33,12 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements OnNavigationItemSelectedListener, Runnable {
 
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
-    private static int CURRENT_PERMISSION;
+//    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+//    private static String[] PERMISSIONS_STORAGE = {
+//            Manifest.permission.READ_EXTERNAL_STORAGE,
+//            Manifest.permission.WRITE_EXTERNAL_STORAGE
+//    };
+//    private static int CURRENT_PERMISSION;
 
     public static final String BASE_DIR = "/storage/sdcard1/PRIVATE/SHARP/CM/MUSIC";
 
@@ -82,10 +82,10 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
         //// デフォルトコード終了
 
         // 権限確認
-        verifyStoragePermissions(this);
-        if (CURRENT_PERMISSION != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
+//        verifyStoragePermissions(this);
+//        if (CURRENT_PERMISSION != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
 
         try {
             // タグ、楽曲表示
@@ -267,19 +267,19 @@ public class MainActivity extends AppCompatActivity implements OnNavigationItemS
         musicPlayerButton.clickLoopButton();
     }
 
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        CURRENT_PERMISSION = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (CURRENT_PERMISSION != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
-    }
+//    public static void verifyStoragePermissions(Activity activity) {
+//        // Check if we have write permission
+//        CURRENT_PERMISSION = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//
+//        if (CURRENT_PERMISSION != PackageManager.PERMISSION_GRANTED) {
+//            // We don't have permission so prompt the user
+//            ActivityCompat.requestPermissions(
+//                    activity,
+//                    PERMISSIONS_STORAGE,
+//                    REQUEST_EXTERNAL_STORAGE
+//            );
+//        }
+//    }
 
     // 戻る押下時処理
     @Override
