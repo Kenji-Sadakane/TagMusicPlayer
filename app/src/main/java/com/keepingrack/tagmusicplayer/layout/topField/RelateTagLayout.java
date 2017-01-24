db11d4850e35ac0b8f5addc4ef80e90cc3456ebf
Kenji.Sadakane <SADA.732738686237@GMAIL.COM>
2017-01-24 20:14:16 +0900

    関連タグ表示処理をリファクタリング

--- app/src/main/java/com/keepingrack/tagmusicplayer/layout/topField/RelateTagLayout.java
+++ app/src/main/java/com/keepingrack/tagmusicplayer/layout/topField/RelateTagLayout.java
@@ -5,44 +5,58 @@ import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
 import com.keepingrack.tagmusicplayer.bean.RelateTag;
-import com.keepingrack.tagmusicplayer.layout.topField.RelateTagTextView.RELATE_TAG_LOCATION;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import static com.keepingrack.tagmusicplayer.MainActivity.DISPLAY_WIDTH;
 import static com.keepingrack.tagmusicplayer.MainActivity.relateTags;
 import static com.keepingrack.tagmusicplayer.layout.RelateTagField.selectedTags;
 import static com.keepingrack.tagmusicplayer.layout.RelateTagField.unselectedTags;
 
+@Getter
+@Setter
 public class RelateTagLayout extends RelativeLayout {
 
-    private int relateTagLineCount = 0;
+    /**
+     * タグTextViewの配置場所
+     * UPPER_LEFT:左上(1個目のタグ)
+     * RIGHT_OF:前のタグの右
+     * NEXT_LINE:次行
+     */
+    public enum RELATE_TAG_LOCATION {UPPER_LEFT, RIGHT_OF, NEXT_LINE}
+
+    // 関連タグ表示行数
+    private int relateTagLineCount = 1;
+    // 全関連タグの表示幅合計
+    private float tagLengthByLine = 0;
+    // 前タグのID
+    private int prevTagId = -1;
 
     public RelateTagLayout(Context context, AttributeSet attr) {
         super(context, attr);
     }
 
+    // 関連タグテキストViewを追加
     public void addRelateTags() {
-        int prevTagId = -1;
-        int tagLengthByLine = 0;
-        relateTagLineCount = 1;
+        initializeField();
         for (RelateTag relateTag : relateTags) {
-            RelateTagTextView tagText = new RelateTagTextView(relateTag.getTag());
-            tagLengthByLine += tagText.getDesiredWidth();
-            RELATE_TAG_LOCATION location = judgeTagTextLocation(tagLengthByLine);
-            tagText.setLayoutParams(location, prevTagId);
-            this.addView(tagText);
-            if (location.equals(RELATE_TAG_LOCATION.NEXT_LINE)) {
-                tagLengthByLine = 0;
-                relateTagLineCount++;
-            }
-            prevTagId = tagText.getId();
+            this.addView(new RelateTagTextView(relateTag.getTag()));
         }
     }
 
+    // 初期化
+    private void initializeField() {
+        relateTagLineCount = 1;
+        tagLengthByLine = 0;
+        prevTagId = -1;
+    }
+
     // 次のタグの配置場所を判定
-    private RELATE_TAG_LOCATION judgeTagTextLocation(int tagLengthByLine) {
+    public RELATE_TAG_LOCATION judgeTagTextLocation(float desiredWidth) {
         RELATE_TAG_LOCATION location = RELATE_TAG_LOCATION.UPPER_LEFT;
         if (this.getChildCount() != 0) {
-            if (tagLengthByLine > DISPLAY_WIDTH - 40) { // 40=tagFieldのmargin(左)+margin(右)
+            if ((tagLengthByLine + desiredWidth) > (DISPLAY_WIDTH - 40)) { // 40=tagFieldのmargin(左)+margin(右)
                 location = RELATE_TAG_LOCATION.NEXT_LINE;
             } else {
                 location = RELATE_TAG_LOCATION.RIGHT_OF;
@@ -51,6 +65,7 @@ public class RelateTagLayout extends RelativeLayout {
         return location;
     }
 
+    // 関連タグを全て削除
     public void removeRelateTagField() {
         this.removeAllViews();
         selectedTags.clear();
@@ -59,6 +74,16 @@ public class RelateTagLayout extends RelativeLayout {
 
     // 関連タグフィールドの表示幅を算出
     public int calcRelateTagFieldHeight() {
-        return relateTagLineCount > 4 ? 300 : relateTagLineCount * 75;
+        return relateTagLineCount > 4 ? 300 : relateTagLineCount * 75 + 15;
+    }
+
+    // 全タグ表示幅合計を更新
+    public void updateTagLengthByLine(float length) {
+        tagLengthByLine += length;
+    }
+
+    // 関連タグ表示行数を加算
+    public void addRelateTagLineCount() {
+        relateTagLineCount += 1;
     }
 }
--- app/src/main/java/com/keepingrack/tagmusicplayer/layout/topField/RelateTagTextView.java
+++ app/src/main/java/com/keepingrack/tagmusicplayer/layout/topField/RelateTagTextView.java
@@ -9,51 +9,50 @@ import android.widget.TextView;
 
 import com.keepingrack.tagmusicplayer.R;
 import com.keepingrack.tagmusicplayer.bean.RelateTag;
+import com.keepingrack.tagmusicplayer.layout.topField.RelateTagLayout.RELATE_TAG_LOCATION;
 
 import static com.keepingrack.tagmusicplayer.MainActivity.activity;
 
 public class RelateTagTextView extends TextView {
 
-    /**
-     * タグTextViewの配置場所
-     * UPPER_LEFT:左上(1個目のタグ)
-     * RIGHT_OF:前のタグの右
-     * NEXT_LINE:次行
-     */
-    public enum RELATE_TAG_LOCATION {UPPER_LEFT, RIGHT_OF, NEXT_LINE}
+    // 親レイアウトへの配置場所
+    private RELATE_TAG_LOCATION location;
 
     public RelateTagTextView(String tag) {
         super(activity);
 
+        // コンテンツ
+        this.setId(View.generateViewId());
+        this.setText(tag);
         // レイアウト
         this.setClickable(true);
         this.setPadding(10, 0, 10, 0); // 左, 上, 右, 下
         this.setBackgroundResource(R.drawable.tag_item);
-//        this.setLayoutParams(createLayoutParams());
-        // コンテンツ
-        this.setId(View.generateViewId());
-        this.setText(tag);
+        this.setLayoutParams(createLayoutParams());
         // リスナー
         this.setOnClickListener(getOnClickListener());
+        // 親レイアウトの情報更新
+        updateRelateTagLayout();
     }
 
-    public void setLayoutParams(RELATE_TAG_LOCATION location, int prevTagId) {
+    private RelativeLayout.LayoutParams createLayoutParams() {
         RelativeLayout.LayoutParams tagTextParams = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         tagTextParams.setMargins(10, 0, 0, 15); // 左, 上, 右, 下
+        location = activity.relateTagLayout.judgeTagTextLocation(getDesiredWidth());
         switch (location) {
             case UPPER_LEFT:
                 break;
             case RIGHT_OF:
-                tagTextParams.addRule(RelativeLayout.ALIGN_TOP, prevTagId);
-                tagTextParams.addRule(RelativeLayout.RIGHT_OF, prevTagId);
+                tagTextParams.addRule(RelativeLayout.ALIGN_TOP, activity.relateTagLayout.getPrevTagId());
+                tagTextParams.addRule(RelativeLayout.RIGHT_OF, activity.relateTagLayout.getPrevTagId());
                 break;
             case NEXT_LINE:
-                tagTextParams.addRule(RelativeLayout.BELOW, prevTagId);
+                tagTextParams.addRule(RelativeLayout.BELOW, activity.relateTagLayout.getPrevTagId());
                 break;
             default:
                 break;
         }
-        this.setLayoutParams(tagTextParams);
+        return tagTextParams;
     }
 
     private View.OnClickListener getOnClickListener() {
@@ -92,6 +91,18 @@ public class RelateTagTextView extends TextView {
         };
     }
 
+    // 親レイアウトの情報更新
+    private void updateRelateTagLayout() {
+        activity.relateTagLayout.setPrevTagId(getId());
+        if (RELATE_TAG_LOCATION.NEXT_LINE.equals(location)) {
+            activity.relateTagLayout.addRelateTagLineCount();
+            activity.relateTagLayout.setTagLengthByLine(getDesiredWidth());
+        } else {
+            activity.relateTagLayout.updateTagLengthByLine(getDesiredWidth());
+        }
+    }
+
+    // 自Viewの横幅算出
     public float getDesiredWidth() {
         return Layout.getDesiredWidth(getText(), getPaint()) + 30;
     }
